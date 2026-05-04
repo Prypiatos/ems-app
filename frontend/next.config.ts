f52fbdea20dfc62acba024012692c5e348bb6fd3
NipunSGeeTH <sangeethnipun385@gmail.com>
2026-05-04 23:12:00 +0530

    feat:set frontend docker file

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const apiProxyTarget = process.env.NEXT_PUBLIC_API_PROXY_TARGET ?? "http://127.0.0.1:8000/api/v1";
 
 const nextConfig: NextConfig = {
+  output: 'export',
   allowedDevOrigins: ["10.10.30.86"],
   async rewrites() {
     return [
